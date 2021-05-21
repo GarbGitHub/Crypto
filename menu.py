8a2114ef2183df3dbeb7f004cd15dbbf1e3b685a
Sergei_Klyuikov <garbich@gmail.com>
2021-05-21 18:50:53 +0300

    added support for different languages

--- menu.py
+++ menu.py
@@ -1,9 +1,11 @@
-def page_menu():
-    """Меню для авторизованных пользователей"""
-    menu = [
-            {"name": "Home", "url": "/"},
-            {"name": "Encrypt", "url": "/encrypt/"},
-            {"name": "Decrypt", "url": "/decrypt/"}
-    ]
+from dict_words import words
 
+
+def page_menu(language):
+    """Меню"""
+    menu = [
+            {"name": words['home'][language], "url": "/"},
+            {"name": words['encrypt'][language], "url": "/encrypt/"},
+            {"name": words['decrypt'][language], "url": "/decrypt/"}
+    ]
     return menu
