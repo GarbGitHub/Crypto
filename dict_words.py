1456aad8f91da66301a114ee871be561b0c3c76c
Sergei_Klyuikov <garbich@gmail.com>
2021-05-21 18:41:41 +0300

    added config_local.py

---
+++ dict_words.py
@@ -0,0 +1,26 @@
+words = {
+    'site_name': {'english': 'Text cryptographer', 'russian': 'Текстовый криптограф'},
+    'site_description': {
+        'english': 'Encrypt or decrypt text using an encryption key.',
+        'russian': 'Зашифровать или расшифровать текст с помощью ключа шифрования.'},
+    'decrypt_description': {
+        'english': 'Decrypt text using an encryption key.',
+        'russian': 'Расшифровать текст с помощью ключа шифрования.'},
+    'encrypt_description': {
+        'english': 'Encrypt text using an encryption key.',
+        'russian': 'Зашифровать текст с помощью ключа шифрования.'},
+    'decrypt': {'english': 'Encrypt', 'russian': 'Расшифровать'},
+    'encrypt': {'english': 'Encrypt', 'russian': 'Зашифровать'},
+    'home': {'english': 'home', 'russian': 'главная'},
+    'encrypt_text': {'english': 'Encrypt text', 'russian': 'Зашифровать текст'},
+    'ciphertext': {'english': 'Ciphertext ', 'russian': 'Зашифрованный текст'},
+    'encrypted_text_and_key': {'english': 'Encrypted text and key', 'russian': 'Зашифрованный текст и ключ'},
+    'decrypted_text_and_key': {'english': 'Decrypted text and key', 'russian': 'Расшифрованный текст и ключ'},
+    'decrypt_text': {'english': 'Decrypt text', 'russian': 'Расшифровать текст'},
+    'text': {'english': 'Text', 'russian': 'Текст'},
+    'generate_new_key': {'english': 'Generate a new key', 'russian': 'Создать новый ключ'},
+    'use_existing_key': {'english': 'Use an existing key', 'russian': 'Использовать существующий ключ'},
+    'existing_key': {'english': 'Existing key', 'russian': 'Существующий ключ'},
+    'key': {'english': 'Key', 'russian': 'Ключ'},
+    'continue_to_encrypt': {'english': 'Continue to encrypt', 'russian': 'Продолжить шифрование'}
+}
