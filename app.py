3bde776ce9756fd13703d977ad0d3441c0f5d762
Sergei_Klyuikov <garbich@gmail.com>
2021-05-21 18:46:42 +0300

    support for different languages

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+
 from cryptography.fernet import Fernet
 from menu import page_menu
-
+from function import session_creation, check_session, session_del
+from dict_words import words
 app = Flask(__name__)
+app.config.from_object('config_local')
 
 
 @app.route('/', methods=["GET"])
 def main():
-    ip = request.headers.get('X-Real-IP')
-    # ip = request.environ['REMOTE_ADDR']
-    print(ip)
-    menu = page_menu()
-    title = 'Text cryptographer'
+    if not check_session():
+        session_creation()
+    language = session['language']
+    menu = page_menu(language)
     return render_template('index.html',
-                           ip=ip,
                            menu=menu,
-                           title=title)
+                           words=words,
+                           title=words['site_name'][language],
+                           description=words['site_description'][language],
+                           language=language)
 
 
 @app.route('/encrypt/', methods=["POST", "GET"])
 def encrypt():
-    menu = page_menu()
+    if not check_session():
+        session_creation()
+    language = session['language']
+    menu = page_menu(language)
     start = True
-    title = 'Encrypt text'
+    title = words['encrypt_text'][language]
+    description = words['encrypt_description'][language]
     if request.method == 'POST':
         my_str = request.form['text']
         type_key = int(request.form['flexRadioDefault'])  # 1 or 2
@@ -45,25 +53,40 @@ def encrypt():
         text = encrypted_text.decode()
         key = byte_key.decode()
         start = False
-        title = 'Cipher text and key'
+        title = words['encrypted_text_and_key'][session['language']]
 
         return render_template('encrypt.html',
                                menu=menu,
                                text=text,
                                key=key,
+                               words=words,
                                title=title,
-                               start=start)
+                               description=description,
+                               language=language,
+                               start=start,
+                               )
 
     return render_template('encrypt.html',
                            menu=menu,
-                           start=start,
-                           title=title)
+                           words=words,
+                           title=title,
+                           description=description,
+                           language=language,
+                           start=start
+                           )
 
 
 @app.route('/decrypt/', methods=["POST", "GET"])
 def decrypt():
-    title = 'Decrypt text'
-    menu = page_menu()
+    if not check_session():
+        session_creation()
+
+    language = session['language']
+    title = words['decrypt_text'][session['language']]
+    description = words['decrypt_description'][language]
+
+    menu = page_menu(language)
+
     if request.method == 'POST':
         byte_key = str.encode(request.form['UserKey'])
         text = str.encode(request.form['text'])
@@ -72,17 +95,28 @@ def decrypt():
         # Result
         text = cipher.decrypt(text).decode()
         key = byte_key.decode()
-        title = 'Decrypted text and key'
+        title = words['decrypted_text_and_key'][session['language']]
 
         return render_template('decrypt.html',
                                menu=menu,
                                text=text,
                                key=key,
+                               words=words,
+                               language=language,
+                               description=description,
                                title=title)
 
     return render_template('decrypt.html',
                            menu=menu,
+                           words=words,
+                           language=language,
+                           description=description,
                            title=title)
+
+
+@app.route('/del/', methods=["GET"])
+def dell():
+    return session_del()
 
 
 if __name__ == '__main__':  # Запуск сервера на локальном устройстве
