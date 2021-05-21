b2191452241df56023fa16dd27feca920fbe681b
Sergei_Klyuikov <garbich@gmail.com>
2021-05-21 19:03:34 +0300

    added file

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-# from config_local import TOKEN_TG, NAME_BOT, WEATHER_URL, KEY_CRYPTO
-#
-# from config_app import TOKEN_TG, NAME_BOT, WEATHER_URL, KEY_CRYPTO
+import os
+
+KEY_STACK = str(os.environ.get("KEY_STACK"))
+SECRET_KEY = str(os.environ.get("SECRET_KEY"))
