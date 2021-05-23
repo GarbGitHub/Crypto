5cc9b7f5cb0e6bf30e857695c1b014f182b51816
Sergei_Klyuikov <garbich@gmail.com>
2021-05-23 21:12:26 +0300

    Spanish language support

--- dict_words.py
+++ dict_words.py
@@ -1,26 +1,107 @@
 words = {
-    'site_name': {'english': 'Text cryptographer', 'russian': 'Текстовый криптограф'},
+    'site_name': {
+        'English': 'Text cryptographer',
+        'Russian': 'Текстовый криптограф',
+        'Spanish': 'Criptógrafo de texto'
+    },
     'site_description': {
-        'english': 'Encrypt or decrypt text using an encryption key.',
-        'russian': 'Зашифровать или расшифровать текст с помощью ключа шифрования.'},
+        'English': 'Encrypt or decrypt text using an encryption key.',
+        'Russian': 'Зашифровать или расшифровать текст с помощью ключа шифрования.',
+        'Spanish': 'Cifre o descifre el texto con la clave de cifrado.'
+    },
     'decrypt_description': {
-        'english': 'Decrypt text using an encryption key.',
-        'russian': 'Расшифровать текст с помощью ключа шифрования.'},
+        'English': 'Decrypt text using an encryption key.',
+        'Russian': 'Расшифровать текст с помощью ключа шифрования.',
+        'Spanish': 'Descifra el texto con la clave de cifrado.'
+    },
     'encrypt_description': {
-        'english': 'Encrypt text using an encryption key.',
-        'russian': 'Зашифровать текст с помощью ключа шифрования.'},
-    'decrypt': {'english': 'Encrypt', 'russian': 'Расшифровать'},
-    'encrypt': {'english': 'Encrypt', 'russian': 'Зашифровать'},
-    'home': {'english': 'home', 'russian': 'главная'},
-    'encrypt_text': {'english': 'Encrypt text', 'russian': 'Зашифровать текст'},
-    'ciphertext': {'english': 'Ciphertext ', 'russian': 'Зашифрованный текст'},
-    'encrypted_text_and_key': {'english': 'Encrypted text and key', 'russian': 'Зашифрованный текст и ключ'},
-    'decrypted_text_and_key': {'english': 'Decrypted text and key', 'russian': 'Расшифрованный текст и ключ'},
-    'decrypt_text': {'english': 'Decrypt text', 'russian': 'Расшифровать текст'},
-    'text': {'english': 'Text', 'russian': 'Текст'},
-    'generate_new_key': {'english': 'Generate a new key', 'russian': 'Создать новый ключ'},
-    'use_existing_key': {'english': 'Use an existing key', 'russian': 'Использовать существующий ключ'},
-    'existing_key': {'english': 'Existing key', 'russian': 'Существующий ключ'},
-    'key': {'english': 'Key', 'russian': 'Ключ'},
-    'continue_to_encrypt': {'english': 'Continue to encrypt', 'russian': 'Продолжить шифрование'}
+        'English': 'Encrypt text using an encryption key.',
+        'Russian': 'Зашифровать текст с помощью ключа шифрования.',
+        'Spanish': 'Cifre el texto con la clave de cifrado.'
+    },
+    'decrypt': {
+        'English': 'Encrypt',
+        'Russian': 'Расшифровать',
+        'Spanish': 'Descifrar'
+    },
+    'encrypt': {
+        'English': 'Encrypt',
+        'Russian': 'Зашифровать',
+        'Spanish': 'Сifrar'
+    },
+    'home': {
+        'English': 'home',
+        'Russian': 'главная',
+        'Spanish': 'página de inicio'
+    },
+    'encrypt_text': {
+        'English': 'Encrypt text',
+        'Russian': 'Зашифровать текст',
+        'Spanish': 'Encriptar texto'
+    },
+    'ciphertext': {
+        'English': 'Ciphertext ',
+        'Russian': 'Зашифрованный текст',
+        'Spanish': 'Texto cifrado'
+    },
+    'encrypted_text_and_key': {
+        'English': 'Encrypted text and key',
+        'Russian': 'Зашифрованный текст и ключ',
+        'Spanish': 'Texto y clave encriptados'
+    },
+    'decrypted_text_and_key': {
+        'English': 'Decrypted text and key',
+        'Russian': 'Расшифрованный текст и ключ',
+        'Spanish': 'Texto y clave descifrados'
+    },
+    'decrypt_text': {
+        'English': 'Decrypt text',
+        'Russian': 'Расшифровать текст',
+        'Spanish': 'Descifrar texto'
+    },
+    'theme': {
+        'English': 'Theme',
+        'Russian': 'Тема',
+        'Spanish': 'Sujeto'
+    },
+    'light': {
+        'English': 'light',
+        'Russian': 'светлая',
+        'Spanish': 'luz'
+    },
+    'dark': {
+        'English': 'dark',
+        'Russian': 'темная',
+        'Spanish': 'oscuro'
+    },
+    'text': {
+        'English': 'Text',
+        'Russian': 'Текст',
+        'Spanish': 'texto'
+    },
+    'generate_new_key': {
+        'English': 'Generate a new key',
+        'Russian': 'Создать новый ключ',
+        'Spanish': 'Crea una nueva clave'
+    },
+    'use_existing_key': {
+        'English': 'Use an existing key',
+        'Russian': 'Использовать существующий ключ',
+        'Spanish': 'Usar una clave existente'
+    },
+    'existing_key': {
+        'English': 'Existing key',
+        'Russian': 'Существующий ключ',
+        'Spanish': 'Clave existente'
+    },
+    'key': {
+        'English': 'Key',
+        'Russian': 'Ключ',
+        'Spanish': 'Clave'
+    },
+    'continue_to_encrypt': {
+        'English': 'Continue encryption',
+        'Russian': 'Продолжить шифрование',
+        'Spanish': 'Continuar con el cifrado'
+    }
 }
